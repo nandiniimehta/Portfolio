7307b64a21a2111944a1e1826c0aa4d0da0a8af3
nandiniimehta <143238234+nandiniimehta@users.noreply.github.com>
2024-07-31 19:28:47 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
         <section id='intro'>
             <div className='introContent'>
                 <span className='hello'>Hello,</span>
-                <span className='introText'>I'm <span className='introName'>{HeaderData.name}</span><br />{HeaderData.roles[0]}</span>
+                <span className='introText'>I'm <span className='introName'>{HeaderData.name}</span><br />{HeaderData.roles[0]} & {HeaderData.roles[1]}</span>
                 <p className='introPar'>{HeaderData.description}</p>
                 <a href={`${process.env.PUBLIC_URL}/NandiniResume.pdf`} download="NandiniResume.pdf" >
                 <button className="btn">Resume</button>
