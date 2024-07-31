8779e4e906c1b0afc988fcad17352c44951ecf2b
nandiniimehta <143238234+nandiniimehta@users.noreply.github.com>
2024-07-31 19:35:06 +0530

    Update Data.js

--- src/components/Data/Data.js
+++ src/components/Data/Data.js
@@ -1,1 +1,51 @@
+export const HeaderData = {
+    name: "Nandini Mehta",
+    roles: [
+        "Web Developer",
+        "Web Designer"
+    ],
+    description: "I am motivated and versatile individual and always eager to take new challenges. I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/nandiniimehta",
+    linkedin: "",
+};
+
+export const SkillData =
+{
+    title1: "Programming Languages",
+    skills1: [
+        "HTML, CSS, JAVASCRIPT, PYTHON, REACTJS, PHP, JAVA, LARAVEL",
+    ],
+    title2: "Database",
+    skills2: [
+        "MYSQL, SQLite ,Microsoft SQL Server",
+    ],
+    title3: "Other",
+    skills3: [
+        "VS CODE, GitHub ",
+    ],
+}
+
+export const Projects = [
+    {
+        title: "Skill Learn",
+        description: "Developed an e-learning website using PHP, MySQL and Bootstrap, providing a platform for students to access online courses and quizzes.",
+        technologies: ["PHP", "HTML", "MYSQL"],
+        // link: "http://localhost/skill%20learn/login.php",
+        github: "https://github.com/nandiniimehta/Skill-Learn-E-learning-website"
+    },
+    {
+        title: "Tic-Tac-Toe",
+        description: "Developed a game where player can play tic-tac-toe and win. I've used reactJS to build it.",
+        technologies: ["React.js", "HTML", "CSS"],
+        // link: "https://tic-tac-toe-three-lyart.vercel.app/",
+        github: "https://github.com/nandiniimehta/Tic-Tac-Toe-"
+    },
+    {
+        title: "Incredible India / AtulyaBharat",
+        description: "It's a static website which represents India's cultural places and their brief description.",
+        technologies: ["React.js", "Bootstrap", "HTML"],
+        // link: "https://incredibleindia.vercel.app/",
+        github: "https://github.com/nandiniimehta/IncredibleIndia"
+    },
+];
 
